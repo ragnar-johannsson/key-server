2dcd6f2f083e871e1ac9be72eae1eb2cf8bddaff
Ragnar B. Johannsson <ragnar@igo.is>
2016-10-10 20:42:59 +0000

    Main entrypoint

---
+++ Server.fs
@@ -0,0 +1,23 @@
+﻿module Server
+
+open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.Logging
+
+let logger = Targets.create Verbose
+
+let router =
+    choose [
+        GET >=> pathScan "/%s" Handlers.getKeys
+        RequestErrors.BAD_REQUEST ""
+    ] >=> Writers.setMimeType "text/plain"
+    >=> log logger logFormat
+
+let config = {
+    defaultConfig with bindings = [ HttpBinding.createSimple HTTP Config.Iface Config.Port ]}
+
+[<EntryPoint>]
+let main argv =
+    startWebServer config router
+    0
