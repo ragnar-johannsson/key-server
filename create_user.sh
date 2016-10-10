d6fec00d12dc480483b515d55fc625320e96c51a
Ragnar B. Johannsson <ragnar@igo.is>
2016-10-10 20:41:08 +0000

    Default user creation script template

---
+++ create_user.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+USER="{{ user }}"
+DEPENDENCIES="sudo useradd"
+
+for DEPENDENCY in $DEPENDENCIES; do
+    if ! which $DEPENDENCY >/dev/null 2>&1; then
+        echo "Error: $DEPENDENCY not found. Exiting..."
+        exit 1
+    fi
+done
+
+echo "Creating user: $USER..."
+
+if ! id $USER >/dev/null 2>&1; then
+
+useradd -m -s /bin/bash -U -G sudo $USER
+mkdir /home/$USER/.ssh
+cat <<EOF > /home/$USER/.ssh/authorized_keys
+{{ keys }}
+EOF
+chown -R $USER:$USER /home/$USER/.ssh
+
+else
+    echo "User $USER already exists. Skipping..."
+fi
